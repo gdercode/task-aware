9cc6a9e7f6514ea7cb50ef88960f17cf2e644f62
Ion Gusenga Derrick <gusenga.derrick@ionplustech.com>
2026-06-11 15:55:20 +0200

    Add detection fields

---
+++ database/migrations/2026_06_11_135406_add_detection_fields_to_flows_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('flows', function (Blueprint $table) {
+            $table->string('source_ip')->nullable();
+            $table->string('destination')->nullable();
+            $table->string('classification')->nullable();
+            $table->bigInteger('bytes')->default(0);
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('flows', function (Blueprint $table) {
+            //
+        });
+    }
+};
